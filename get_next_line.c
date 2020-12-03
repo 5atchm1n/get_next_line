a9bb2b6bef253011c29e4fdf4dbc88ad1dae30b4
sshakya <sshakya@student.42.fr>
2020-12-03 23:39:42 +0100

    Tyring : some ideas

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,24 @@
 /*   By: sshakya <sshakya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 12:17:24 by sshakya           #+#    #+#             */
-/*   Updated: 2020/12/02 12:32:58 by sshakya          ###   ########.fr       */
+/*   Updated: 2020/12/03 23:38:37 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_next.h"
+
+int				get_next_line(int fd, char **line)
+{
+	t_list		*buffer;		
+	size_t		i;
+
+	i = fd;
+	buffer = NULL;
+	
+	if (!buffer)
+
+
+	buffer.p_read = read(fd, buffer, count);
+
+	return(0);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,22 @@
 /*   By: sshakya <sshakya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 12:17:18 by sshakya           #+#    #+#             */
-/*   Updated: 2020/12/02 12:17:34 by sshakya          ###   ########.fr       */
+/*   Updated: 2020/12/03 23:38:37 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+# include <unistd.h>
+
+typedef struct		s_list
+{
+	ssize_t			p_read;
+	char			line[BUFFER SIZE + 1];
+}					t_list;
+
+
+int					get_next_line(int fd, char **line);
+
+#endif

8977b9484f6aa95e95f5669ab6173434dc4496a0
sshakya <sshakya@student.42.fr>
2020-12-17 19:11:26 +0100

    :heavy_check_mark: Added malloc protection
    *
    
    Due to 25 lines per function list function had to be split again
    Added protection in struct malloc
    Added protection in ft_strdup malloc
    
    Norm check done
    *

--- get_next_line.c
+++ get_next_line.c
@@ -6,24 +6,30 @@
 /*   By: sshakya <sshakya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 12:17:24 by sshakya           #+#    #+#             */
-/*   Updated: 2020/12/17 17:39:41 by sshakya          ###   ########.fr       */
+/*   Updated: 2020/12/17 19:08:25 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static t_list		*ft_set_gnline(t_list *gnline, int fd)
+static t_list		*ft_set_head(t_list *gnline, int fd)
 {
 	if (gnline == NULL)
 	{
 		gnline = malloc(sizeof(t_list));
+		if (!gnline)
+			return (NULL);
 		gnline->ifd = fd;
 		gnline->buff = ft_strdup("");
 		gnline->head = gnline;
 		gnline->next = NULL;
-		return (gnline);
 	}
 	gnline = gnline->head;
+	return (gnline);
+}
+
+static t_list		*ft_get_gnline(t_list *gnline, int fd)
+{
 	while (gnline->next != NULL)
 	{
 		if (gnline->ifd == fd)
@@ -33,6 +39,8 @@ static t_list		*ft_set_gnline(t_list *gnline, int fd)
 	if (gnline->ifd == fd)
 		return (gnline);
 	gnline->next = malloc(sizeof(t_list));
+	if (!gnline->next)
+		return (NULL);
 	gnline->next->ifd = fd;
 	gnline->next->buff = ft_strdup("");
 	gnline->next->head = gnline->head;
@@ -66,6 +74,8 @@ static int			ft_return(int n, char **buff, char **line, char **tmp)
 	if (n == 0 && ft_strlen(*buff) == 0)
 	{
 		*line = ft_strdup("");
+		if (!line)
+			return (-1);
 		return (0);
 	}
 	*tmp = ft_set_line(*buff, line);
@@ -83,9 +93,10 @@ int					get_next_line(int fd, char **line)
 	char			*tmp;
 	int				n;
 
-	gnline = ft_set_gnline(gnline, fd);
+	gnline = ft_set_head(gnline, fd);
+	gnline = ft_get_gnline(gnline, fd);
 	if ((read(fd, buffer, 0) < 0) || fd == 0 || !line || BUFFER_SIZE < 1
-	|| !gnline)
+	|| !gnline || !gnline->buff)
 		return (-1);
 	while ((n = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{

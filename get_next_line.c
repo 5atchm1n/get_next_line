58c2eaf7bfd94dd59c2273e43b2fa65128a9fded
sshakya <sshakya@student.42.fr>
2020-12-23 08:17:47 +0100

    :rocket: creating space for free list
    *
    
    added room for one more function :smile:
    
    *

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,79 @@
 /*   By: sshakya <sshakya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 12:17:24 by sshakya           #+#    #+#             */
-/*   Updated: 2020/12/23 06:13:34 by sshakya          ###   ########.fr       */
+/*   Updated: 2020/12/23 08:17:28 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+/* FAILED FUNCTION ?!!!!!!!! */
+/*
+static t_list		*ft_set_head(t_list *gnline, int fd)
+{
+	if (gnline == NULL)
+	{
+		gnline = malloc(sizeof(t_list));
+		if (!gnline)
+			return (NULL);
+		gnline->ifd = fd;
+		gnline->buff = ft_strdup("");
+		gnline->head = gnline;
+		gnline->next = NULL;
+	}
+	gnline = gnline->head;
+	return (gnline);
+}
+
+static t_list		*ft_get_gnline(t_list *gnline, int fd)
+{
+	while (gnline->next != NULL)
+	{
+		if (gnline->ifd == fd)
+			return (gnline);
+		gnline = gnline->next;
+	}
+	if (gnline->ifd == fd)
+		return (gnline);
+	gnline->next = malloc(sizeof(t_list));
+	if (!gnline->next)
+		return (NULL);
+	gnline->next->ifd = fd;
+	gnline->next->buff = ft_strdup("");
+	gnline->next->head = gnline->head;
+	gnline->next->next = NULL;
+	return (gnline->next);
+}
+*/
+
+static t_list		*ft_set_gnline(t_list *gnline, int fd)
+{
+	if (gnline == NULL)
+	{
+		gnline = malloc(sizeof(t_list));
+		gnline->ifd = fd;
+		gnline->buff = ft_strdup("");
+		gnline->head = gnline;
+		gnline->next = NULL;
+		return (gnline);
+	}
+	gnline = gnline->head;
+	while (gnline->next != NULL)
+	{
+		if (gnline->ifd == fd)
+			return (gnline);
+		gnline = gnline->next;
+	}
+	if (gnline->ifd == fd)
+		return (gnline);
+	gnline->next = malloc(sizeof(t_list));
+	gnline->next->ifd = fd;
+	gnline->next->buff = ft_strdup("");
+	gnline->next->head = gnline->head;
+	gnline->next->next = NULL;
+	return (gnline->next);
+}
+
 
 static char			*ft_set_line(char *str, char **line)
 {
@@ -39,26 +107,87 @@ static char			*ft_set_line(char *str, char **line)
 	return (nbuffer);
 }
 
-int					get_next_line(int fd, char **line)
+/* FAIL FUNCTION ?!!!!                    */
+/*
+static char			*ft_set_line(char *str, char **line)
 {
-	static char		*buffer;
-	char			tmp[BUFFER_SIZE + 1];
-	char			*buff;
-	int				n;
+	char			*nbuffer;
+	size_t			i;
 
-	if ((read(fd, tmp, 0) < 0) || fd == 0 || !line || BUFFER_SIZE < 1)
-		return (-1);
-	buff = "";
-	while ((n = read(fd, tmp, BUFFER_SIZE)) > 0)
+	i = 0;
+	while (str[i] != '\n' && str[i] != '\0')
+		i++;
+	if (str[i] == '\n')
 	{
-		tmp[n] = '\0';
-		buff = ft_strjoin(tmp, buff);
-		if (ft_strchr(buff, '\n'))
-			break ;
+		*line = ft_substr(str, 0, i);
+		nbuffer = ft_strdup(&str[i + 1]);
 	}
-	buffer = ft_set_line(buff, line);
-	free(buff);
-	if (n == 0 && ft_strlen(buffer) == 0)
+	if (str[i] == '\0')
+	{
+		*line = ft_substr(str, 0, i);
+		nbuffer = ft_strdup("");
+	}
+	return (nbuffer);
+}
+*/
+/*FAILED FUNCTION ?!!!! */
+/*
+static int			ft_return(int n, char **buff, char **line, char **tmp)
+{
+	if (n == 0 && ft_strlen(*buff) == 0)
+	{
+		*line = ft_strdup("");
+		if (!line)
+			return (-1);
+		return (0);
+	}
+	*tmp = ft_set_line(*buff, line);
+	free(*buff);
+	*buff = *tmp;
+	if (!buff)
+		return (-1);
+	return (1);
+}
+*/
+static int			ft_return(int n, t_list *data, char **line, char **tmp)
+{
+	if (!tmp)
+		return (-1);
+	*tmp = ft_set_line(data->buff, line);
+	free(data->buff);
+	data->buff = *tmp;
+	if (data->buff == NULL)
+		return (-1);
+	if (n == 0 && ft_strlen(data->buff) == 0)
 		return (0);
 	return (1);
+}
+int					get_next_line(int fd, char **line)
+{
+	static t_list	*gnline;
+	char			buffer[BUFFER_SIZE + 1];
+	char			*tmp;
+	int				n;
+/* FAILED FUNCTION ?!!!!! */
+/*
+	gnline = ft_set_head(gnline, fd);
+	gnline = ft_get_gnline(gnline, fd);
+*/
+	gnline = ft_set_gnline(gnline, fd);
+	if ((read(fd, buffer, 0) < 0) || fd == 0 || !line || BUFFER_SIZE < 1
+	|| !gnline || !gnline->buff)
+		return (-1);
+	while ((n = read(fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		buffer[n] = '\0';
+		tmp = ft_strjoin(gnline->buff, buffer);
+		free(gnline->buff);
+		gnline->buff = tmp;
+		if (!tmp)
+			return (-1);
+		if (ft_strchr(gnline->buff, '\n'))
+			break ;
+	}
+	n = ft_return(n, gnline, line, &tmp);
+	return (n);
 }

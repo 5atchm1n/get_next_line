16fe7268d718e82897d0c66611a387371088ce67
sshakya <sshakya@student.42.fr>
2020-12-23 23:03:51 +0100

    :rocket: trying using fixed size array
    *
    
    *

--- get_next_line.c
+++ get_next_line.c
@@ -6,83 +6,36 @@
 /*   By: sshakya <sshakya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 12:17:24 by sshakya           #+#    #+#             */
-/*   Updated: 2020/12/23 08:17:28 by sshakya          ###   ########.fr       */
+/*   Updated: 2020/12/23 23:01:37 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/* FAILED FUNCTION ?!!!!!!!! */
-/*
-static t_list		*ft_set_head(t_list *gnline, int fd)
+static int			ft_getfd(t_list *gnline, int fd)
 {
-	if (gnline == NULL)
+	int				n;
+
+	n = 0;
+	if (gnline[n].ifd == 0)
 	{
-		gnline = malloc(sizeof(t_list));
-		if (!gnline)
-			return (NULL);
-		gnline->ifd = fd;
-		gnline->buff = ft_strdup("");
-		gnline->head = gnline;
-		gnline->next = NULL;
+		gnline[n].ifd = fd;
+		gnline[n].buff = "";
+		return(n);
 	}
-	gnline = gnline->head;
-	return (gnline);
+	while (gnline[n].ifd != 0)
+	{
+		if (gnline[n].ifd == fd)
+			return (n);
+		n++;
+	}
+	gnline[n].ifd = fd;
+	gnline[n].buff = "";
+	return (n);
 }
 
-static t_list		*ft_get_gnline(t_list *gnline, int fd)
+static char			*ft_set_line(char *str, char **line, char **tmp)
 {
-	while (gnline->next != NULL)
-	{
-		if (gnline->ifd == fd)
-			return (gnline);
-		gnline = gnline->next;
-	}
-	if (gnline->ifd == fd)
-		return (gnline);
-	gnline->next = malloc(sizeof(t_list));
-	if (!gnline->next)
-		return (NULL);
-	gnline->next->ifd = fd;
-	gnline->next->buff = ft_strdup("");
-	gnline->next->head = gnline->head;
-	gnline->next->next = NULL;
-	return (gnline->next);
-}
-*/
-
-static t_list		*ft_set_gnline(t_list *gnline, int fd)
-{
-	if (gnline == NULL)
-	{
-		gnline = malloc(sizeof(t_list));
-		gnline->ifd = fd;
-		gnline->buff = ft_strdup("");
-		gnline->head = gnline;
-		gnline->next = NULL;
-		return (gnline);
-	}
-	gnline = gnline->head;
-	while (gnline->next != NULL)
-	{
-		if (gnline->ifd == fd)
-			return (gnline);
-		gnline = gnline->next;
-	}
-	if (gnline->ifd == fd)
-		return (gnline);
-	gnline->next = malloc(sizeof(t_list));
-	gnline->next->ifd = fd;
-	gnline->next->buff = ft_strdup("");
-	gnline->next->head = gnline->head;
-	gnline->next->next = NULL;
-	return (gnline->next);
-}
-
-
-static char			*ft_set_line(char *str, char **line)
-{
-	char			*nbuffer;
 	size_t			i;
 
 	i = 0;
@@ -91,103 +44,60 @@ static char			*ft_set_line(char *str, char **line)
 	if (str[0] == '\0')
 	{
 		*line = NULL;
-		nbuffer = ft_strdup("");
-		return (nbuffer);
+		return (NULL);
 	}
 	if (str[i] == '\n')
 	{
 		*line = ft_substr(str, 0, i);
-		nbuffer = ft_strdup(&str[i + 1]);
+		*tmp = &str[i + 1];
 	}
 	if (str[i] == '\0')
 	{
 		*line = ft_substr(str, 0, i);
-		nbuffer = ft_strdup("");
+		*tmp = "";
 	}
-	return (nbuffer);
+	return (*tmp);
 }
 
-/* FAIL FUNCTION ?!!!!                    */
-/*
-static char			*ft_set_line(char *str, char **line)
-{
-	char			*nbuffer;
-	size_t			i;
 
-	i = 0;
-	while (str[i] != '\n' && str[i] != '\0')
-		i++;
-	if (str[i] == '\n')
-	{
-		*line = ft_substr(str, 0, i);
-		nbuffer = ft_strdup(&str[i + 1]);
-	}
-	if (str[i] == '\0')
-	{
-		*line = ft_substr(str, 0, i);
-		nbuffer = ft_strdup("");
-	}
-	return (nbuffer);
-}
-*/
-/*FAILED FUNCTION ?!!!! */
-/*
-static int			ft_return(int n, char **buff, char **line, char **tmp)
+static int			ft_return(int n, char **fdbuff, char **line, char **tmp)
 {
-	if (n == 0 && ft_strlen(*buff) == 0)
+	*tmp = ft_set_line(*fdbuff, line, tmp);
+	if (n == 0 && ft_strlen(*fdbuff) == 0)
 	{
-		*line = ft_strdup("");
-		if (!line)
-			return (-1);
+		free(*tmp);
+		tmp = NULL;
 		return (0);
 	}
-	*tmp = ft_set_line(*buff, line);
-	free(*buff);
-	*buff = *tmp;
-	if (!buff)
+	*fdbuff = *tmp;
+	if (*fdbuff == NULL)
 		return (-1);
 	return (1);
 }
-*/
-static int			ft_return(int n, t_list *data, char **line, char **tmp)
-{
-	if (!tmp)
-		return (-1);
-	*tmp = ft_set_line(data->buff, line);
-	free(data->buff);
-	data->buff = *tmp;
-	if (data->buff == NULL)
-		return (-1);
-	if (n == 0 && ft_strlen(data->buff) == 0)
-		return (0);
-	return (1);
-}
+
 int					get_next_line(int fd, char **line)
 {
-	static t_list	*gnline;
+	static t_list	gnline[FDMAX];
 	char			buffer[BUFFER_SIZE + 1];
 	char			*tmp;
 	int				n;
-/* FAILED FUNCTION ?!!!!! */
-/*
-	gnline = ft_set_head(gnline, fd);
-	gnline = ft_get_gnline(gnline, fd);
-*/
-	gnline = ft_set_gnline(gnline, fd);
-	if ((read(fd, buffer, 0) < 0) || fd == 0 || !line || BUFFER_SIZE < 1
-	|| !gnline || !gnline->buff)
+	int				ifd;
+	int				start;
+
+	if ((read(fd, buffer, 0) < 0) || fd == 0 || !line || BUFFER_SIZE < 1)
 		return (-1);
+	ifd = ft_getfd(gnline, fd);
+	n = 0;
+	start = 1;
 	while ((n = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
 		buffer[n] = '\0';
-		tmp = ft_strjoin(gnline->buff, buffer);
-		free(gnline->buff);
-		gnline->buff = tmp;
-		if (!tmp)
-			return (-1);
-		if (ft_strchr(gnline->buff, '\n'))
+		tmp = ft_strjoin(gnline[ifd].buff, buffer);
+		gnline[ifd].buff = tmp;
+		start = 0;
+		if (ft_strchr(gnline[ifd].buff, '\n'))
 			break ;
 	}
-	n = ft_return(n, gnline, line, &tmp);
+	n = ft_return(n, &gnline[ifd].buff, line, &tmp);
 	return (n);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,32 +6,32 @@
 /*   By: sshakya <sshakya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 12:17:18 by sshakya           #+#    #+#             */
-/*   Updated: 2020/12/23 06:03:22 by sshakya          ###   ########.fr       */
+/*   Updated: 2020/12/23 23:02:20 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 #ifndef FDMAX
-# define FDMAX 256
-#endif
-
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 256
+#define FDMAX 16
 #endif
 
 # include <unistd.h>
-# include <stdio.h>
 # include <stdlib.h>
+# include <stdio.h>
+
+typedef struct		s_list
+{
+	int				ifd;
+	char			*buff;
+	struct s_list	*next;
+}					t_list;
 
 int					get_next_line(int fd, char **line);
-char				*ft_strjoin(char const *s1, char const *s2);
-char				*ft_strdup(const char *src);
+char				*ft_strjoin(char *s1, char *s2);
 char				*ft_strchr(const char *str, int c);
-size_t				ft_strlen(const char *s);
-void				*ft_memcpy(void *dest, const void *src, size_t n);
 char				*ft_substr(char const *s, unsigned int start, size_t len);
+size_t				ft_strlen(const char *s);
 
 #endif

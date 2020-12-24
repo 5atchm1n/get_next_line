34128d56bf1acdbd5ed310e082576ae56ba4828d
sshakya <sshakya@student.42.fr>
2020-12-24 06:46:48 +0100

    :heavy_check_mark: No Leaks - Norm check done
    *
    
    Fully functional and put to the Norm.
    
    Valgrind check also done
    *

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,36 @@
 /*   By: sshakya <sshakya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 12:17:24 by sshakya           #+#    #+#             */
-/*   Updated: 2020/12/24 04:00:27 by sshakya          ###   ########.fr       */
+/*   Updated: 2020/12/24 06:38:32 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static int			gnl_check(char **line, char *gnline)
+{
+	if (!line || !gnline)
+		return (-1);
+	return (1);
+}
+
+static int			gnl_return(char **line, char **gnline)
+{
+	int				ret;
+
+	ret = 0;
+	if (*gnline)
+		*line = *gnline;
+	else
+	{
+		*line = malloc(sizeof(char) * 1);
+		if (*line == NULL)
+			return (-1);
+		*line[0] = '\0';
+	}
+	*gnline = NULL;
+	return (ret);
+}
 
 int					get_next_line(int fd, char **line)
 {
@@ -24,7 +49,8 @@ int					get_next_line(int fd, char **line)
 	while ((n = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
 		buffer[n] = '\0';
-		gnline = gnl_join(gnline, buffer);
+		if (!(gnline = gnl_join(gnline, buffer)))
+			return (-1);
 		flag = gnl_find(gnline);
 		if (flag != -1)
 			break ;
@@ -34,14 +60,7 @@ int					get_next_line(int fd, char **line)
 	{
 		*line = gnl_sub(gnline, 0, flag, 0);
 		gnline = gnl_dup(&gnline[flag + 1], gnline);
-		return (1);
+		return (gnl_check(line, gnline));
 	}
-	if (gnline)
-	{
-		*line = gnl_sub(gnline, 0, gnl_len(gnline), 1);
-		gnline = NULL;
-		return (0);
-	}
-	*line = NULL;
-	return (0);
+	return (gnl_return(line, &gnline));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: sshakya <sshakya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 12:17:18 by sshakya           #+#    #+#             */
-/*   Updated: 2020/12/24 04:01:24 by sshakya          ###   ########.fr       */
+/*   Updated: 2020/12/24 06:46:29 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 128
+# endif
 
 # include <unistd.h>
 # include <stdlib.h>
@@ -20,7 +24,8 @@
 int					get_next_line(int fd, char **line);
 char				*gnl_join(char *s1, char *s2);
 int					gnl_find(char *str);
-char				*gnl_sub(const char *s, unsigned int start, size_t len, int n);
+char				*gnl_sub(const char *s, unsigned int start, size_t len,
+							int n);
 size_t				gnl_len(const char *s);
 char				*gnl_dup(const char *src, const char *srcfree);
 
